eb2bf83ecfb947a00f28a5baeff5c16477385624
assafvan <vanunuassaf@gmail.com>
2021-12-03 11:55:27 +0200

    third-commit

--- playScreen.js
+++ playScreen.js
@@ -1,4 +1,17 @@
-const playScreen = document.querySelector(".play-screen");
+const playScreen = document.querySelector(".game__playScreen");
+const landingPage = document.querySelector(".landingPage");
+const startBtn = document.querySelector(".landingPage__startBtn");
+const resetBtn = document.querySelector(".sideBar__resetScreen");
+let inventory = document.querySelector(".sideBar__inventory");
+const pickAxe = document.querySelector(".sideBar__pickAxe");
+const shovel = document.querySelector(".sideBar__shovel");
+const axe = document.querySelector(".sideBar__axe");
+
+let theGame = {
+  selectedTool: "",
+  clickedOnInventory: false,
+  isEmptyInventory: true,
+};
 
 const blocksObj = {
   0: "stone",
@@ -19,7 +32,7 @@ const blocksMatrix = [
   [6, 6, 4, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6],
   [6, 4, 4, 4, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6],
   [6, 4, 4, 4, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 4, 6, 6, 6],
-  [6, 6, 3, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 4, 6, 6, 6],
+  [6, 6, 3, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 4, 4, 4, 6, 6],
   [6, 6, 3, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 3, 6, 6, 6],
   [2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
@@ -33,35 +46,70 @@ const blocksMatrix = [
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 ];
 
-export function draw() {
+function draw() {
   for (let i = 0; i < blocksMatrix.length; i++) {
     for (let j = 0; j < blocksMatrix[i].length; j++) {
       const block = document.createElement("div");
       block.classList.add(blocksObj[blocksMatrix[i][j]]);
+      block.setAttribute(`row`, i);
+      block.setAttribute(`column`, j);
       playScreen.appendChild(block);
     }
   }
 }
 
-// const blocksMatrix = [
-//     ["none","none","cloud","cloud","none","none","none","none","none","none","none","none","none","none","none","none","none","none","none","none",],
-//     ["none","cloud","cloud", "none","none","none","none","none","none","none","none","none","none","none","cloud","cloud","none","none","none","none",],
-//     ["none","none","none", "none","none","none","none","none","none","none","none","none","none","cloud","cloud","cloud","cloud","none","none","none",],
-//     ["none","none","none", "none","none","none","none","none","none","none","none","none","none","none","none","none","none","none","none","none",],
-//     ["none","none","leaves", "none","none","none","none","none","none","none","none","none","none","none","none","none","none","none","none","none",],
-//     ["none","leaves","leaves", "leaves","none","none","none","none","none","none","none","none","none","none","none","none","none","none","none","none",],
-//     ["none","leaves","leaves", "leaves","none","none","none","none","none","none","none","none","none","none","none","none","none","none","none","none",],
-//     ["none","none","log", "none","none","none","none","none","none","none","none","none","none","none","none","none","none","none","none","none",],
-//     ["none","none","log", "none","none","none","none","none","none","none","none","none","none","none","none","none","none","none","none","none",],
-//     ["grass","grass","dirt", "grass","grass","grass","grass","grass","grass","grass","grass","grass","grass","grass","grass","grass","grass","grass","grass","grass",],
-//     ["dirt","dirt","dirt", "dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt",],
-//     ["dirt","dirt","dirt", "dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt",],
-//     ["dirt","dirt","dirt", "dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt",],
-//     ["stone","stone","stone", "stone","stone","stone","stone","stone","stone","stone","stone","stone","stone","stone","stone","stone","stone","stone","stone","stone",],
-//     ["stone","stone","stone", "stone","stone","stone","stone","stone","stone","stone","stone","stone","stone","stone","stone","stone","stone","stone","stone","stone",],
-//     ["stone","stone","stone", "stone","stone","stone","stone","stone","stone","stone","stone","stone","stone","stone","stone","stone","stone","stone","stone","stone",],
-//     ["stone","stone","stone", "stone","stone","stone","stone","stone","stone","stone","stone","stone","stone","stone","stone","stone","stone","stone","stone","stone",],
-//     ["stone","stone","stone", "stone","stone","stone","stone","stone","stone","stone","stone","stone","stone","stone","stone","stone","stone","stone","stone","stone",],
-//     ["stone","stone","stone", "stone","stone","stone","stone","stone","stone","stone","stone","stone","stone","stone","stone","stone","stone","stone","stone","stone",],
-//     ["stone","stone","stone", "stone","stone","stone","stone","stone","stone","stone","stone","stone","stone","stone","stone","stone","stone","stone","stone","stone",],
-// ];
+startBtn.addEventListener("click", () => {
+  landingPage.classList.toggle("display-none");
+  playScreen.classList.toggle("display-none");
+  //   inventory.classList.toggle("display-none");
+  draw();
+});
+
+resetBtn.addEventListener("click", (e) => {
+  playScreen.innerHTML = "";
+  draw();
+  resetToolsClasses();
+});
+
+pickAxe.addEventListener("click", () => {
+  theGame.selectedTool = "pickaxe";
+  pickAxe.classList.add("active");
+});
+
+playScreen.addEventListener("click", (e) => {
+  switch (theGame.selectedTool) {
+    case "pickaxe":
+      console.log(theGame.selectedTool);
+
+      console.log(e.target.classList.value);
+
+      if (e.target.classList.value === blocksObj[0]) {
+        e.target.classList.remove("stone");
+        e.target.classList.add("none");
+        // inventory.classList = "";
+        inventory.classList.add("stone");
+      } else {
+        flashRed();
+      }
+  }
+});
+
+function flashRed() {
+  let selectedTool;
+  if (theGame.selectedTool === "pickaxe") selectedTool = pickAxe;
+  if (theGame.selectedTool === "shovel") selectedTool = shovel;
+  if (theGame.selectedTool === "axe") selectedTool = axe;
+  selectedTool.classList.add("red");
+  setTimeout(function () {
+    selectedTool.classList.remove("red");
+  }, 100);
+}
+
+function resetToolsClasses() {
+  pickAxe.classList = "";
+  pickAxe.classList.add("sideBar__pickAxe", "btn");
+  shovel.classList = "";
+  shovel.classList.add("sideBar__shovel", "btn");
+  axe.classList = "";
+  axe.classList.add("sideBar__axe", "btn");
+}
